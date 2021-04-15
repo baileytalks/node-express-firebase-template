65fd01f8405ed5620f209920fc47c6433d6e8a39
Bailey Kursar <bkursar@gmail.com>
2021-04-15 09:53:10 +0100

    Adding error to a branch for visibility

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 const express = require('express');
+const expressMd = require('express-md');
 const app = express();
 const port = 3000;
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/public/index.html');
+// create an instance of express-md with custom options
+var mdRouter = expressMd({
+
+  // serve markdown files from `docs` directory
+  dir: __dirname + '/public',
+
+  // serve requests from root of the site
+  url: '/',
+
+  headers: {
+    'Content-Type': 'text/html; charset=utf-8'
+  }
+
+  // variables to replace {{{ varName }}} in markdown files
+  // vars: {
+  //   message: 'Hello World!'
+  // }
 });
+
+// add as express middleware
+app.use(mdRouter);
 
 app.get('*', function(req, res){
   res.sendFile(__dirname + '/public/404.html');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(port, function () {
+  console.log(expressMd.version + ' listening on port ' + port);
 });
